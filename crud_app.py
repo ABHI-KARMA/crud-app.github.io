e0ff9733008edefab9c0a277ec79971509b92c57
ABHISHEK VISHWAKARMA <73927352+ABHI-KARMA@users.noreply.github.com>
2021-06-04 12:04:33 +0530

    second commit

--- crud_app.py
+++ crud_app.py
@@ -82,18 +82,6 @@ def update(id):
     return render_template('update.html', employee = employee)
  
  
-# @app.route('/delete/<int:id>', methods=['GET','POST'])
-# def delete(id):
-#     employee = EmployeeModel.query.filter_by(employee_id=id).first()
-#     if request.method == 'POST':
-#         if employee:
-#             db.session.delete(employee)
-#             db.session.commit()
-#             return redirect('/data')
-#         abort(404)
- 
-#     return render_template('delete.html')
- 
 @app.route('/delete/<int:id>')
 def delete(id):
     employee = EmployeeModel.query.filter_by(employee_id=id).first()
